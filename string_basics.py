696dd042c6c7b5eade6fb0406cb61de53228b1ab
Example Student <example1@students.hasanprep.org>
2020-09-14 16:22:58 +0000

    Changed assignment

--- string_basics.py
+++ string_basics.py
@@ -4,7 +4,7 @@ name = 'My name is Simon Escalada-Mastick.'
 degrees = 'I studied linguistics and mathematics in college.'
 occupation = 'I am a data analyst and teacher at HPLS.'
 city = 'I live in Tucson, Arizona.'
-favorite_restaurant = 'My favorite restaurant in Tucson is Yoshimatsu.'
+favorite_restaurant = "My favorite restaurant in Tucson is BK's."
 
 # Print the five variables.
 print(name)
