25fb4392c0486002c7bc92cdb2be47000d690869
ExampleStudent1 <70875804+ExampleStudent1@users.noreply.github.com>
2020-09-14 09:20:11 -0700

    Completed assignment

--- string_basics.py
+++ string_basics.py
@@ -1,5 +1,14 @@
 # string_basics.py
 # Declare FIVE variables with strings that describe yourself.
-
+name = 'My name is Simon Escalada-Mastick.'
+degrees = 'I studied linguistics and mathematics in college.'
+occupation = 'I am a data analyst and teacher at HPLS.'
+city = 'I live in Tucson, Arizona.'
+favorite_restaurant = 'My favorite restaurant in Tucson is Yoshimatsu.'
 
 # Print the five variables.
+print(name)
+print(degrees)
+print(occupation)
+print(city)
+print(favorite_restaurant)
