adeb84f1afaad5061d67ff57a0c970ebea7f9f8b
ExampleStudent1 <70875804+ExampleStudent1@users.noreply.github.com>
2020-09-16 09:12:08 -0700

    Update string_basics.py

--- string_basics.py
+++ string_basics.py
@@ -4,11 +4,7 @@ name = 'My name is Simon Escalada-Mastick.'
 degrees = 'I studied linguistics and mathematics in college.'
 occupation = 'I am a data analyst and teacher at HPLS.'
 city = 'I live in Tucson, Arizona.'
-<<<<<<< HEAD
 favorite_restaurant = 'My favorite restaurant in Tucson is Taco Bell.'
-=======
-favorite_restaurant = "My favorite restaurant in Tucson is BK's."
->>>>>>> 696dd042c6c7b5eade6fb0406cb61de53228b1ab
 
 # Print the five variables.
 print(name)
